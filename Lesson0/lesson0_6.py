b2c5f2de11a1c35630f1e7a501ee5137b2387fc0
otaniteruaki <otani@trend-bull.com>
2021-05-10 16:47:37 +0900

    課題0のインプット反映済み

--- Lesson0/lesson0_6.py
+++ Lesson0/lesson0_6.py
@@ -1,7 +1,6 @@
 def name_check(name):
   name_list=["たんじろう","ぎゆう","ねずこ","むざん"]
-  check_result = name in name_list
-  if check_result == True:
+  if name in name_list:
     print(name + "は存在します。")
   else:
     print(name + "は存在しません。")
